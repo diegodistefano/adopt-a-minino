46571ad104085fd75e26a9c4ac11221abdaa402a
diegodistefano <diego.distefano.1989@gmail.com>
2025-03-11 14:25:36 +0100

    Conect to api

---
+++ src/service/penguinService.js
@@ -0,0 +1,12 @@
+const URL_API = "https://api.thecatapi.com/v1/images/search?limit=10";
+
+
+async function getPenguins() {
+    const response = await fetch(URL_API);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+
+getPenguins();

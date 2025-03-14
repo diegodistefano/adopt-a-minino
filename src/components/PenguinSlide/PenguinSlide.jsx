dffed23195a6fbcdeb45917891246ab72e491e88
diegodistefano <diego.distefano.1989@gmail.com>
2025-03-14 12:41:23 +0100

    next butty

--- src/components/PenguinSlide/PenguinSlide.jsx
+++ src/components/PenguinSlide/PenguinSlide.jsx
@@ -3,25 +3,44 @@ import "./PenguinSlide.css";
 import { useState, useEffect } from "react";
 import { getPenguins } from "../../service/penguinService";
 import PenguinCard from "../PenguinCard/PenguinCard.jsx";
+import PenguinButton from "../PenguinButton/PenguinButton.jsx";
 
 const PenguinSlide = () => {
   const [dataPenguin, setDataPenguin] = useState([]);
+  const [indexImage, setIndexImage] = useState(0);
+  const [penguinDisplayed, setPenguinDisplayed] = useState([]);
 
   useEffect(() => {
     const loadData = async () => {
       const dataPenguin = await getPenguins();
       setDataPenguin(dataPenguin);
+
+      setPenguinDisplayed(dataPenguin.slice(indexImage, indexImage + 3));
+      setIndexImage(indexImage + 3);
+      
     };
+
+    if(indexImage > 0){
+      return;
+    }
     loadData();
   }, []);
 
+  const nextImages = () => {
+    console.log(penguinDisplayed);
+    setPenguinDisplayed(dataPenguin.slice(indexImage, indexImage + 3));
+    setIndexImage(indexImage + 3);
+    console.log(penguinDisplayed);
+  }
+
   return (
     <div className="container-carrusel">
-      {dataPenguin.map((penguin) => (
+      {penguinDisplayed.map((penguin) => (
         <>
         <PenguinCard urlImagen = {penguin.url} tipo = "planta" nombre= "Pinguino de planta"/>
         </>
       ))}
+      <PenguinButton text="next" onClick={nextImages}/>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-
 import PenguinSlide from "./../components/PenguinSlide/PenguinSlide";
 import PenguinCard from "./../components/PenguinCard/PenguinCard";
 import PenguinButton from "./../components/PenguinButton/PenguinButton";
 
 const HomePage = () => {
   return (
-    <>
+    <div className="container-home">
       <h2>Esta es la home page</h2>
       <PenguinSlide />
-    </>
+    </div>
   );
 };
 

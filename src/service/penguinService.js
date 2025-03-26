399db8d905c5800edc255bc89d0d279563aac0fd
sarayminar <sarayminar@gmail.com>
2025-03-26 10:25:34 +0100

    feat: adding key api

--- src/service/penguinService.js
+++ src/service/penguinService.js
@@ -1,8 +1,19 @@
-const URL_API = "https://api.thecatapi.com/v1/images/search?limit=10";
+const API_KEY = "live_kg452T34tBZDb4oN4wyKXi6QA8VukzL4WhepLyeQB0srFoRbz1XIPi9oHSE6UNcI";
+const URL_API = "https://api.thecatapi.com/v1/images/search?limit=10&include_breeds=true";
+
+
 
 export async function getPenguins() {
-    const response = await fetch(URL_API);
+  console.log(API_KEY);
+    const response = await fetch(URL_API, {
+      method: "GET",
+      headers: {
+       'x-api-key':API_KEY,
+        "Content-Type": "application/json",
+      },
+    })
     const data = await response.json();
     console.log(data);
     return data;
 }
+
